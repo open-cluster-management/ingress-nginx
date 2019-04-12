0145166e18e032bb10e8d7718959323d8447f35c
Todd E. Johnson <toddjohn@us.ibm.com>
2019-04-11 19:46:51 -0500

    Fix ssl_certifcate path in nginx-kube.conf for impersonation (#65)
    The nginx-kube.conf file was hardcoding the name of the ssl secret path
    to router_certs.  In 3.2 a new secret name is used.  Modified the code
    to use the --default-ssl-certificate parameter for the secret name.

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -7,8 +7,12 @@ then
        echo "APISERVER_SECURE_POrt not set, using default."
        APISERVER_SECURE_PORT="8001"
    fi
+   # Get the name of SSL secret to use from the command line parm
+   SECRET_PARM=$(getopt -u -q -l default-ssl-certificate: $@)
+   SECRET_FILE="$(echo ${SECRET_PARM} | cut -d' ' -f2 | tr / -)"
    # Update the nginx template to include the server for kubernetes. 
    sed -i "s/{{APISERVER_SECURE_PORT}}/${APISERVER_SECURE_PORT}/g" /opt/ibm/router/nginx/conf/impersonation/nginx-kube.conf
+   sed -i "s/{{SECRET_FILE}}/${SECRET_FILE}/g" /opt/ibm/router/nginx/conf/impersonation/nginx-kube.conf
    sed -i '/http {/a \    include /opt/ibm/router/nginx/conf/impersonation/nginx-kube.conf;' /opt/ibm/router/nginx/template/nginx.tmpl
    # Get the public key from the cert that signs the id tokens.  This is used to verify the id token is valid. 
    openssl x509 -pubkey -noout -in /var/run/secrets/platform-auth/tls.crt > /var/run/secrets/platform-auth-public.pem
